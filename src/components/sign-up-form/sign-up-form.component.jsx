630395d9130707e89734e519a963d6e76fd8ae88
yogita <sabaley16@gmail.com>
2024-10-08 19:05:01 +0530

    'added'

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../firebase/utils/firebase.utils'
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+import { UserContext } from "../../contexts/user.context";
 
 const SignUpForm = () => {
     const defaultFormFields = {
@@ -12,7 +13,11 @@ const SignUpForm = () => {
     }
     const [formFields, setFormFields] = useState(defaultFormFields)
     const { displayName, email, password, confirmPassword } = formFields
+    
 
+    const {setCurrentUser} = useContext(UserContext)
+    console.log('hit');
+    
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     };
@@ -30,7 +35,7 @@ const SignUpForm = () => {
                 email,
                 password
             );
-
+            setCurrentUser(user)
             await createUserDocumentFromAuth(user, { displayName });
             resetFormFields();
         } catch (error) {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,24 +1,41 @@
-import { Outlet , Link } from "react-router-dom";
-import React from "react";
-import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+import { Outlet, Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import './navigation.styles.scss'
-
+import { UserContext } from '../../contexts/user.context';
+import { SignOutUser } from "../../firebase/utils/firebase.utils";
 const Navigation = () => {
-    return (
-        <React.Fragment>
-          <div className="navigation">
-            <Link to='/' className="logo-container">
-                {/* <div>Logo</div> */}
-                <CrownLogo className="logo"/>
+  const { currentUser,setCurrentUser } = useContext(UserContext)
+  console.log(currentUser);
+  
+  const SignOutHandler = async () => {
+   await SignOutUser()
+   
+    setCurrentUser(null)
+  }
+  return (
+    <React.Fragment>
+      <div className="navigation">
+        <Link to='/' className="logo-container">
+          {/* <div>Logo</div> */}
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to='/shop' className='nav-link'>Shop</Link>
+          {currentUser ? (
+            <span className='nav-link' onClick={SignOutHandler} >
+              {' '}
+              SIGN OUT{' '}
+            </span>
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
             </Link>
-            <div className="nav-links-container">
-                <Link to='/shop' className='nav-link'>Shop</Link>
-                <Link to='/auth' className='nav-link'>Sign In</Link>
-            </div>
-          </div>
-          <Outlet/>
-        </React.Fragment>
-      )
+          )}        </div>
+      </div>
+      <Outlet />
+    </React.Fragment>
+  )
 }
 
 export default Navigation
