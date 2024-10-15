8c6fb85260235952a1c3a0479a236b7656691e74
yogita <sabaley16@gmail.com>
2024-10-15 18:57:02 +0530

    checkout page added'

--- src/components/cart-dropdown/cart-dropown.component.jsx
+++ src/components/cart-dropdown/cart-dropown.component.jsx
@@ -1,11 +1,17 @@
 import Button from '../button/button.component';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
+import { useNavigate } from 'react-router-dom';
 const CartDropdown = () => {
-
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate()
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout')
+  }
+  
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
@@ -18,7 +24,7 @@ const CartDropdown = () => {
         )}
 
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   );
 };
