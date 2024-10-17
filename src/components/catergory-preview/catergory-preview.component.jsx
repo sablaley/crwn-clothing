f210245fe7916e799eb7418537c08e0c85cca0aa
yogita <sabaley16@gmail.com>
2024-10-17 19:17:29 +0530

    updated shop page

--- src/components/catergory-preview/catergory-preview.component.jsx
+++ src/components/catergory-preview/catergory-preview.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import '../category-preview.styles.scss'
+import './category-preview.styles.scss'
+import ProductCard from '../../components/product-card/product-card-component'
 
 const CategoryPreview = ({ title, products }) => {
     return (
         <div className="category-preview-container">
             <h2>
-                <span>{title.toUppercase()}</span>
+                <span>{title.toUpperCase()}</span>
             </h2>
             <div className='preview'>
                 {products.filter((_, idx) => idx < 4).map((product) => (
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React, { useContext,Fragment} from 'react'
 import './shop.styles.scss';
 // import SHOP_DATA from '../../shop-data.json';
 import { CategoriesContext } from '../../contexts/categories.context'
-import ProductCard from '../../components/product-card/product-card-component'
+import CategoryPreview from '../../components/catergory-preview/catergory-preview.component';
 
 const Shop = () => {
     const { categoriesMap } = useContext(CategoriesContext)
@@ -14,16 +14,8 @@ const Shop = () => {
         <Fragment>
            {
             Object.keys(categoriesMap).map((title)=>{
-                return (
-                    <Fragment key={title}>
-                        <h2>{title}</h2>
-                        <div className="products-container">
-                            {categoriesMap[title].map((product)=>(
-                                <ProductCard key={product.id} product={product} />
-                            ))}
-                        </div>
-                    </Fragment>
-                )
+                const products = categoriesMap[title]
+                return <CategoryPreview key={title} title={title} products={products} />
             })
            }
         </Fragment>
