bff3577c1332884d7f833e5fe6b4656a80612754
yogita <sabaley16@gmail.com>
2024-10-17 16:11:23 +0530

    js file added

--- src/firebase/utils/firebase.utils.js
+++ src/firebase/utils/firebase.utils.js
@@ -11,7 +11,7 @@ import {
   signOut,
   onAuthStateChanged
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query} from 'firebase/firestore';
 
 
 // Your web app's Firebase configuration
@@ -36,6 +36,19 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey,objectsToAdd) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+  
+  objectsToAdd.forEach((object) => {
+     const docRef = doc(collectionRef, object.title.toLowerCase());
+     batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+}
 
 export const createUserDocumentFromAuth = async (userAuth,additionalInformation = {}) => {
   if (!userAuth) return;
